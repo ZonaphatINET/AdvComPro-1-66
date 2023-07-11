380f401fe8d2a1b6a4329aba8d266fac4d75e7fd
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-07-11 09:18:29 +0700

    up

---
+++ L02/6. literals.js
@@ -0,0 +1,13 @@
+console.log("\n***Concatenation with template liteals***\n")
+
+let str1 = "javaScript";
+let str2 = "fun";
+
+console.log(`I a, wriying code in ${str1}`);
+console.log(`Formatting in ${str1} is ${str2}!`);
+
+console.log("\n***Expressions in template literals***\n");
+
+let bool1 = true;
+console.log(`1 + 1 is ${1 + 1}`);
+console.log(`The opposite of true is ${!bool1}`);
