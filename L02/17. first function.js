d50994a57313864d4acf6d45d51d24e3253aa3c0
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-07-11 10:49:50 +0700

    update

---
+++ L02/17. first function.js
@@ -0,0 +1,9 @@
+function printHello(name){
+    console.log("Hello " + name);
+    return name + "Hello ";
+}
+
+console.log(printHello.name);
+
+let result = printHello("Naphat");
+console.log(result);
