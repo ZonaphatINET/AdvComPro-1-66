50e13a4d5e73285d3e0bda0efcd91726ea220ec7
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-07-11 11:07:43 +0700

    up

---
+++ L02/20. Arrow function exam;le.js
@@ -0,0 +1,17 @@
+const sun = (firstParma, secondParam) => {
+    return firstParma + secondParam;
+}
+console.log(sun(2, 5));
+
+const printHello = () => {
+    console.log("Hello");
+};
+printHello();
+
+const checkWeight = (weight) => {
+    console.log(`Baggage weight : ${weight} kilograms.`);
+}
+checkWeight(25);
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 30));
