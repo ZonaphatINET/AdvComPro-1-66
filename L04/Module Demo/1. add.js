e2fd674c72d48676de68f14bba38e6e4f2ff83d2
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-07-25 08:58:46 +0700

    update

---
+++ L04/Module Demo/1. add.js
@@ -0,0 +1,8 @@
+const hello = require('./1. libHello');
+
+hello.sayHello();
+console.log(hello.person.name);
+console.log(hello.person.weigth);
+console.log(hello.cube(3));
+console.log(hello.add(5, 4));
+console.log(hello.status);
---
+++ L04/Module Demo/2. add Area.mjs
@@ -0,0 +1,4 @@
+import { areaCircle, areaSquare } from "./2. libArea.mjs";
+
+console.log(areaCircle(5));
+console.log(areaSquare(10,6));
