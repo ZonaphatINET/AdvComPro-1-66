d78d58911739c0ccaf6cb74ba417ee352610c9d9
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-09-12 08:16:45 +0700

    update

--- L07/HW/HW.js
+++ L07/HW/HW.js
@@ -1,24 +1,18 @@
-function findMaxIndices(numbers) {
-    const maxIndices = [];
-    
-    for (let i = 0; i < numbers.length; i++) {
-      if (i === 0 || numbers[i] > numbers[i - 1]) {
-        maxIndices.push(i);
-      }
+const nums1 = [3, 2, 3, 1];
+const nums2 = [1, 2, 1, 3, 5, 6, 4];
+const nums3 = [1, 2, 1, 4, 5, 6, 4, 7];
+
+function findPeaksIndices(nums) {
+    const peakIndices = [];
+
+    for (let i = 1; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+            peakIndices.push(i);
+        }
     }
-    
-    return maxIndices;
-  }
-  
-  const numbers1 = [3, 2, 3, 1];
-  const numbers2 = [1, 2, 1, 3, 5, 6, 4];
-  const numbers3 = [1, 2, 1, 4, 5, 6, 4, 7];
-  
-  const maxIndices1 = findMaxIndices(numbers1);
-  const maxIndices2 = findMaxIndices(numbers2);
-  const maxIndices3 = findMaxIndices(numbers3);
-  
-  console.log("numbers1: ตำแหน่งที่มีค่ามากที่สุดก่อนลดลงคือ index " + maxIndices1.join(", "));
-  console.log("numbers2: ตำแหน่งที่มีค่ามากที่สุดก่อนลดลงคือ index " + maxIndices2.join(", "));
-  console.log("numbers3: ตำแหน่งที่มีค่ามากที่สุดก่อนลดลงคือ index " + maxIndices3.join(", "));
-  
+    return peakIndices;
+}
+
+console.log(`Peak element index of : ${nums1.join(',')} -> ${findPeaksIndices(nums1).join(',')}`);
+console.log(`Peak element index of : ${nums2.join(',')} -> ${findPeaksIndices(nums2).join(',')}`);
+console.log(`Peak element index of : ${nums3.join(',')} -> ${findPeaksIndices(nums3).join(',')}`);
